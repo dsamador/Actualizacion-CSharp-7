d7b72b2a77e85126f755c19f6a0abf44a35c79c9
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-14 14:47:29 -0500

    Literales numéricas

---
+++ 01 Literales numericas/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int valor = 1_556_731;
+int doblar = valor * 2;
+Console.WriteLine($"{valor}, * 2 = {doblar}");
+
+//podemos especificar numeros en binario con el prefijo 0b
+var binario = 0b10010_1001;
+Console.WriteLine($"El valor colocado es {binario}");
+
