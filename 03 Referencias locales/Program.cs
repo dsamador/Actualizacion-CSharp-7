5c6d5794814225537ba57d1b3ef3d002cddfe5a1
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-14 15:46:20 -0500

    Ref locals

---
+++ 03 Referencias locales/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+//Ref Locals
+
+//Nos permite tener una variable local de referencia a un
+//elemento de un arreglo
+
+string[] palabras = { "Hola", "a", "todos", "mis", "amigos" };
+
+ref string referencia = ref palabras[2];
+
+Console.WriteLine($"Por indice palabras[2] : {palabras[2]}");
+Console.WriteLine($"Por referencia : {referencia}");
+Console.WriteLine("--------");
+referencia += " completos";
+
+Console.WriteLine($"Por indice palabras[2] : {palabras[2]}");
+Console.WriteLine($"Por referencia : {referencia}");
+
+Console.WriteLine("--------");
+
+foreach(string palabra in palabras)
+    Console.WriteLine(palabra);
+Console.WriteLine("--------");
+
+referencia = "";
+
+foreach (string palabra in palabras)
+    Console.WriteLine(palabra);
