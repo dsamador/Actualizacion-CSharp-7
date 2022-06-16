6b880c161a71cbbb507db1e9a24e644afc354bdf
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-15 20:33:07 -0500

    Fat arrow y métodos locales

---
+++ 06 Atualizacion a metodos/Program.cs
@@ -0,0 +1,13 @@
+﻿
+static double sumar(double x, double y) => x * y;
+
+static void numeros()
+{
+    Console.WriteLine(DobleLength("Hola"));
+    Console.WriteLine(DobleLength("a todos"));
+    Console.WriteLine(DobleLength("mis amigos"));
+
+    int DobleLength(string s) => s.Length * 2;
+}
+
+numeros();
