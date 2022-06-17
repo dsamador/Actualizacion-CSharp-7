1a1b7cb5a02c5aaf911af3c9f3e631f9377d8c81
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-16 19:39:16 -0500

    Deconstructor

---
+++ 07 Deconstructor y destructor/Clase.cs
@@ -0,0 +1,30 @@
+﻿namespace _07_Deconstructor_y_destructor
+{
+    internal class Clase
+    {
+        private string nombre;
+        private int edad;
+
+        public Clase(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public override string ToString()
+        {
+            return $"{nombre} tiene {edad} de edad";
+        }
+
+        //Debe llevar siempre la palabra Deconstruct
+        public void Deconstruct(out string oNombre, out int oEdad)
+        {
+            //Mandamos al exterior lo que nos interesa antes
+            //de que el objeto sea destruido.
+            oNombre = nombre;
+            oEdad = edad;
+        }
+        //Creamos el destructor que usa =>
+        ~Clase() => Console.WriteLine("Adios");
+    }
+}
